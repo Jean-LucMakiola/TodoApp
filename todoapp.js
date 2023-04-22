151b22560ab951a0ae1bf6f7e796caa1b78ffbd6
JeanLucMakiola <makiolajeanluc@gmail.com>
2023-04-22 18:30:31 +0200

    2nd Commit

--- todoapp.js
+++ todoapp.js
@@ -1,24 +1,34 @@
-const folderWrapper = document.getElementsByClassName('folders');
+const folderWrapper = document.getElementById('folder-holder');
+
+
+
 function newFolder(){
-    console.log('clicked')
     const folder = document.createElement('div');
-    
-
     folder.appendChild(newFolderHeader());
-    console.log(folder);
-
+    folder.classList.add('folder');
     folderWrapper.appendChild(folder);
-    
-    console.log(folderWrapper);
 }
+
 function newFolderHeader(){
     const folderHeader = document.createElement('header');
     const para = document.createElement('p');
     para.innerHTML = 'name';
+    para.setAttribute('contenteditable', 'true');
     const button = document.createElement('button');
     button.innerHTML = 'delete';
+    button.setAttribute('onclick', 'deleteFolder()');
 
     folderHeader.appendChild(para);
     folderHeader.appendChild(button);   
     return folderHeader;
+}
+function newFolderContent(){
+
+}
+
+function deleteFolder(ele){
+    const thisFolder = ele.parentElement;
+    console.log(thisFolder);
+    thisFolder.style.backgroundcolor = "red";
+    //folderWrapper.remove(thisFolder);
 }
